88dca2346c4fec25dd75f642fda5b3cce44d5dde
Shintaro Katayama <shintaro.katayama@gmail.com>
2014-02-13 14:44:13 +0100

    support tophat-2.0.10 and ruby-2.1.0

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-module load bowtie/1.0.0
-module load ruby/2.1.0-preview1
+
+module load bioinfo-tools
 module load samtools/0.1.19
-module load tophat/2.0.9
+module load bowtie/1.0.0
+module load tophat/2.0.10
+module load ruby/2.1.0
 
 export GEM_HOME=$PWD/var/lib/gems
 export RUBYLIB=$GEM_HOME
 export PATH=$GEM_HOME/bin:$PATH
-export TMPDIR=$HOME/opt/tmp
