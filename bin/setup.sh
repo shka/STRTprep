73e0db2e676fb5bd216c7bd0fb45c739b10a0272
Shintaro Katayama <shintaro.katayama@gmail.com>
2014-06-10 14:14:32 +0200

    bug fix in setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,7 +7,7 @@ module load tophat/2.0.10
 module load R/3.0.1
 module load openmpi/1.7
 module load ruby/2.1.0
-module load BEDTools/2.17.0
+module load bedtools/2.17.0
 module load ucsc/140527
 
 export GEM_HOME=$PWD/var/lib/gems
