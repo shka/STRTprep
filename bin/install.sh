2b79eb0250f5efafe50495038179bcc7a538719f
Shintaro Katayama <shintaro.katayama@gmail.com>
2015-04-08 11:21:21 +0200

    temporary commit; use stable cufflinks

--- bin/install.sh
+++ bin/install.sh
@@ -2,12 +2,17 @@
 
 set -e
 
+mkdir -p bin/cufflinks-2.1.1
 if ! [ -d '.homebrew' ]; then
     if [ `uname` = 'Linux' ]; then
 	git clone https://github.com/Homebrew/linuxbrew.git .homebrew
+	# cufflinks 2.1.1; 2.2.1 was unstable
+	curl -s http://cole-trapnell-lab.github.io/cufflinks/assets/downloads/cufflinks-2.1.1.Linux_x86_64.tar.gz | tar -C ./bin/cufflinks-2.1.1 --strip-component 1 -zxvf -
     else
 	mkdir .homebrew
 	curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C .homebrew
+	# cufflinks 2.1.1; 2.2.1 was unstable
+	curl -s http://cole-trapnell-lab.github.io/cufflinks/assets/downloads/cufflinks-2.1.1.OSX_x86_64.tar.gz | tar -C ./bin/cufflinks-2.1.1 --strip-component 1 -zxvf -
     fi
 fi
 
@@ -23,6 +28,7 @@ git --work-tree .homebrew/Library/Taps/homebrew/homebrew-science --git-dir .home
 brew install bedtools
 
 brew install ruby
+brew install homebrew/versions/boost155
 brew install coreutils
 brew install fastq-tools
 brew install parallel
@@ -30,8 +36,7 @@ brew install pigz
 brew install gawk
 brew install samtools-0.1
 brew install bowtie
-brew install tophat
-brew install cufflinks
+brew install tophat --with-bowtie
 brew install kent-tools
 brew install R --with-openblas --without-tcltk --without-x11
 
@@ -45,3 +50,5 @@ library(devtools)
 install_github('shka/samr', ref='test_multblock')
 install_github('shka/R-SAMstrt')
 EOF
+
+
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export LC_ALL=C
-export PATH=.homebrew/bin:$PATH
+export PATH=.homebrew/bin:./bin/cufflinks-2.1.1:$PATH
