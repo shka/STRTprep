077c1bbfe013379fc337a23e562bda7079a073bd
Shintaro Katayama <shintaro.katayama@gmail.com>
2015-04-14 12:39:51 +0200

    bug fix; unexpected leader sequence for ERCC-00130

--- bin/_prepare_ercc92.sh
+++ bin/_prepare_ercc92.sh
@@ -3,4 +3,4 @@
 curl -o src/ERCC92.zip 'https://tools.lifetechnologies.com/content/sfs/manuals/ERCC92.zip'
 
 mkdir -p tmp
-unzip -cq src/ERCC92.zip ERCC92.fa | gawk 'BEGIN{p=""} /^>/{ print p ">RNA_SPIKE_" substr($1, 2); printf("GGGGGAATTC") } /^[ACGT]/{ printf $1; p="\n" } END{ print }' | gfold -w 50 | gzip -c > tmp/ercc92.fa.gz
+unzip -cq src/ERCC92.zip ERCC92.fa | gawk 'BEGIN{p=""} /^>/{ print p ">RNA_SPIKE_" substr($1, 2); printf("GGGGGAATTC" ($1 == ">ERCC-00130" ? "GAGCTC" : "") ) } /^[ACGT]/{ printf $1; p="\n" } END{ print }' | gfold -w 50 | gzip -c > tmp/ercc92.fa.gz
