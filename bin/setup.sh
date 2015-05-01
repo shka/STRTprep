dcc42c567542a7443627916dd947cf84b63b487a
Shintaro Katayama <shintaro.katayama@gmail.com>
2015-05-01 14:55:19 +0200

    bug fix; optimize for linux

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export LC_ALL=C
-export PATH=.homebrew/bin:./bin/cufflinks-2.1.1:$PATH
-export HOMEBREW_CACHE=.homebrew/cache
+export PATH=$PWD/.homebrew/bin:$PWD/bin/cufflinks-2.1.1:$PATH
+export HOMEBREW_CACHE=$PWD/.homebrew/cache
