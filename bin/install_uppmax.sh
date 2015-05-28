ae8e8bb4424a6cbd6843ed7f4caeaa584d705c44
Shintaro Katayama <shintaro.katayama@gmail.com>
2015-05-28 13:43:17 +0200

    update for UPPMAX

--- bin/install_uppmax.sh
+++ bin/install_uppmax.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+. bin/setup_uppmax.sh
+
 if ! [ -d '.homebrew' ]; then
     git clone https://github.com/Homebrew/linuxbrew.git .homebrew
 fi
-
-. bin/setup_uppmax.sh
+## Issue: Unable to bootstrap gcc
+##   https://github.com/Homebrew/linuxbrew/issues/137
+ln -s `which gcc` \
+   `brew --prefix`/bin/gcc-`gcc -dumpversion | cut -d. -f1,2` && true
+ln -s `which g++` \
+   `brew --prefix`/bin/g++-`g++ -dumpversion | cut -d. -f1,2` && true
+ln -s `which gfortran` \
+   `brew --prefix`/bin/gfortran-`gfortran -dumpversion | cut -d. -f1,2` && true
 
 brew tap homebrew/science
 brew update
@@ -19,8 +27,12 @@ gem install rake
 
 R --vanilla --quiet <<EOF
 source("http://bioconductor.org/biocLite.R")
-biocLite(c('Heatplus', 'devtools', 'samr', 'yaml'), ask=F)
+biocLite(c('devtools', 'samr', 'yaml'), ask=F)
 library(devtools)
 install_github('shka/samr', ref='test_multblock')
 install_github('shka/R-SAMstrt')
 EOF
+
+gcc -o bin/_step1b_fastq2oneLine bin/_step1b_fastq2oneLine.c
+gcc -o bin/_step1b_trimWithQCFilter bin/_step1b_trimWithQCFilter.c
+
