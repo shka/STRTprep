506c71df4aac8b063a5853ccd38c8febb8bca603
Shintaro Katayama <shintaro.katayama@gmail.com>
2016-07-29 12:12:53 +0200

    minor; update of local utility

--- bin/__seqcp.rb
+++ bin/__seqcp.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
-
 require 'yaml'
 conf = YAML.load_file(ARGV[0])
 
-conf['LIBS'].each do |lib, subconf|
+system "mkdir -p src/raw"
+
+conf['LIBRARIES'].each do |lib, subconf|
   subconf['FASTQS'].each_index do |i|
     src = subconf['FASTQS'][i]
     cols = src.split(/\/+/)
-    dest = "src/#{cols[4]}-#{cols[8]}"
-    system "scp -p milou:#{src} #{dest}"
+    dest = "src/raw/#{cols[4]}-#{cols[8]}"
+    system "cp --preserve=timestamps #{src} #{dest}"
     subconf['FASTQS'][i] = dest
   end
 end
 
-outfp = open('conf.converted.yaml', 'w')
+outfp = open('src/conf.converted.yaml', 'w')
 YAML.dump(conf, outfp)
 outfp.close
